2849a3072a48b923d63393adcbc4177e8879354a
loriter <mingalinglee@gmail.com>
2014-03-18 15:09:59 -0700

    Updated files to work with LP51

--- Main Code.c
+++ Main Code.c
@@ -2,21 +2,21 @@
 #include <math.h>
 #include <stdlib.h>
 //#include <dos.h>
-#include <at89lp828.h>
+#include <at89lp51rd2.h>
 
 // Left Wheel: Forward = 3.2 ON 3.3 Off Backward = 3.2 Off 3.3 ON
 // Right Wheel Forward = 3.4 ON 3.5 OFF Backward = 3.4 Off 3.5 ON
 
-#define CLK 3686400L
+#define CLK 22118400L
 #define BAUD 115200L
-#define TIMER_2_RELOAD (0x10000L-(CLK/(16L*BAUD)))
+#define BRG_VAL (0x100L-(CLK/(32L*BAUD)))
 #define FREQ 100L
-#define TIMER0_RELOAD_VALUE (65536L-(CLK/FREQ))
+#define TIMER0_RELOAD_VALUE (65536L-(CLK/(12L*FREQ)))
 
 //#define RequiredDistance 2.0 // Sets the distance to be 2 meters ( for now )
 #define MOVEMENTSPEED 10 // 10 cm/s
 #define TURNSPEED	1	 // Angle / s
-double RequiredDistance =1; // Sets the distance to be 2 meters ( for now )
+double RequiredDistance = 1; // Sets the distance to be 2 meters ( for now )
 double RequiredAngle = 90;
 
 double x=0,y=0;
@@ -69,91 +69,122 @@ double ReferenceVoltage(void);//Gets the peak voltage for the reference signal.
 							  //We can use these functions to help us get the distance.
 double SignalVoltage(void);   //Same as above function except for the signal voltage. 
 
+unsigned char _c51_external_startup(void)
+{
+	// Configure ports as a bidirectional with internal pull-ups.
+	P0M0=0;	P0M1=0;
+	P1M0=0;	P1M1=0;
+	P2M0=0;	P2M1=0;
+	P3M0=0;	P3M1=0;
+	AUXR=0B_0001_0001; // 1152 bytes of internal XDATA, P4.4 is a general purpose I/O
+	P4M0=0;	P4M1=0;
+    
+    // Initialize the serial port and baud rate generator
+    PCON|=0x80;
+	SCON = 0x52;
+    BDRCON=0;
+    BRL=BRG_VAL;
+    BDRCON=BRR|TBCK|RBCK|SPD;
+    
+    return 0;
+    
+}
 
 int main( void )
-{
-	
-    int running = 1;     // Keeps the program running
-     //setbaud_timer2(TIMER_2_RELOAD); // Initialize the timer
+{  
      
-     double DistanceToSignal,     // Determines the distance the tether is from the signal
-            AngleToSignal;        // Determines what angle the signal is in relation to the tether's right side
+	double DistanceToSignal,     // Determines the distance the tether is from the signal
+			AngleToSignal;        // Determines what angle the signal is in relation to the tether's right side
      
-	 startupTest(); //test the circuit before beginning
+	startupTest(); //test the circuit before beginning
 
      //Nested while loops ensure that the car is in lined up before moving forward or back
      //IMPORTANT: This is assuming that when you change the angle of the car the signal will also change the distance factor
      
-     setbaud_timer2(TIMER_2_RELOAD);
-     
-     while( running )
-          {
-               // Determine distance and angle function
-               AngleToSignal = DetermineAngleToSignal();
-               DistanceToSignal = DetermineDistanceToSignal();
+	while( 1 )  // Keeps the program running
+	{
+		// Determine distance and angle function
+		AngleToSignal = DetermineAngleToSignal();
+		DistanceToSignal = DetermineDistanceToSignal();
                
-               while( (abs(SignalVoltage()-ReferenceVoltage()) < 0.2)&&(SignalVoltage() > 0.05) )
-               
-                    {
-                    	StopMoving();
-                    	DistanceToSignal = DetermineDistanceToSignal();
+		while( (abs(SignalVoltage()-ReferenceVoltage()) < 0.2)&&(SignalVoltage() > 0.05) )
+		{
+			StopMoving();
+			DistanceToSignal = DetermineDistanceToSignal();
                     	
-                    		//IMPORTANT: This is assuming that when you change the angle of the car the signal will also change the distance factor
-                          while( ( DistanceToSignal > (RequiredDistance +0.2 )) || (  DistanceToSignal < (RequiredDistance-0.2) ) )	{
+			//IMPORTANT: This is assuming that when you change the angle of the car the signal will also change the distance factor
+			while( ( DistanceToSignal > (RequiredDistance +0.2 )) || (  DistanceToSignal < (RequiredDistance-0.2) ) )
+			{
+				MoveDistance( DistanceToSignal );     // Moves a distance towards the tether distance
+				//delay();
+				DistanceToSignal = (ReferenceVoltage() + SignalVoltage())/2;
+			}
                           
-		                          MoveDistance( DistanceToSignal );     // Moves a distance towards the tether distance
-		                          //delay();
-				
-		                          DistanceToSignal = (ReferenceVoltage() + SignalVoltage())/2;
-		                    }
+			StopMoving();
+			//AngleToSignal = DetermineAngleToSignal();//end of while loop to keep or break the conditions
+		}
                           
-                          StopMoving();
-                          //AngleToSignal = DetermineAngleToSignal();//end of while loop to keep or break the conditions
-                    }
-                          
-                if(ReferenceVoltage() < SignalVoltage() )
-                {
-                	while(((SignalVoltage()-ReferenceVoltage()) > 0.2) ){
-              		TurnLeft();
-              		}
+		if(ReferenceVoltage() < SignalVoltage() )
+		{
+			while(((SignalVoltage() - ReferenceVoltage()) > 0.2) )
+			{
+				TurnLeft();
+			}
               		
-              		StopMoving();
-              	}
-              	else if (SignalVoltage()< ReferenceVoltage())
-              	{
-              		while(( (ReferenceVoltage() -SignalVoltage()) >0.2)){
-              		TurnRight();
-              		}
+			StopMoving();
+		}
+		
+		else if (SignalVoltage() < ReferenceVoltage())
+		{
+			while(( (ReferenceVoltage()-SignalVoltage()) > 0.2))
+			{
+				TurnRight();
+			}
               		
-              		StopMoving();
-              	}  
-                        
+			StopMoving();
+		}          
                 
-                else if(((abs(SignalVoltage()-ReferenceVoltage())) < 0.2) && (SignalVoltage() < 0.05 ))
-                {
-                	while((SignalVoltage() < 0.1) &&(ReferenceVoltage() <0.1)){
-                	TurnRight();   
-                	}
+		else if(((abs(SignalVoltage() - ReferenceVoltage())) < 0.2) && (SignalVoltage() < 0.05 ))
+		{
+			while((SignalVoltage() < 0.1) && (ReferenceVoltage() < 0.1))
+			{
+				TurnRight();   
+			}
                 	
-                	StopMoving();
+			StopMoving();
                 
-                }
-                
-                
-               //ProperAnglePosition(AngleToSignal); //move vehicle to proper angle
-               
-               if (P2_6 == 0) //increase distance (top left botton pin)
-               		RequiredDistance += 0.5;
+		}    
+		//ProperAnglePosition(AngleToSignal); //move vehicle to proper angle
+        
+        //Task Move Farther
+		if (P2_3 == 0)
+		{
+			RequiredDistance += 0.5;
                		//increaseDistance();
-               if (P2_7 == 0) //decrease distance (second pin from top left)
-           			if (RequiredDistance <= 0.5)
-           				RequiredDistance -= 0.5;
+		}
+		//Task Move Closer
+		if (P2_4 == 0)
+		{
+			if (RequiredDistance <= 0.5)
+			{
+				RequiredDistance -= 0.5;
+			}
            			//decreaseDistance();
-          }
-     return 0;
+		}
+		//*TODO*: Task 180degrees
+		if (P2_5 == 0)
+		{
+		}
+		//*TODO*: Task Parallel Park
+		if (P2_6 == 0)
+		{
+		}
+	}
+
 }
 
-void startupTest(void){
+void startupTest(void)
+{
 	
 	MoveForward();
 	testDelay();
@@ -235,22 +266,22 @@ void TurnLeft( void )
 } 
 
 void MoveDistance( double DistanceToSignal )
-     {
+{
           
-          if( DistanceToSignal > RequiredDistance )
-	              {
-	                   MoveForward();// Move towards the signal
-	                   //delay( MovementTimeRequired);//this might have to be inside the moving functions
-					   //StopMoving();
-	              }
-	         else if( DistanceToSignal < RequiredDistance )
-	              {
-	              	   MoveBackward();// Move away from signal
-	                   //delay( MovementTimeRequired);//this might have to be inside the moving functions
-					   //StopMoving();
-	              }
+	if( DistanceToSignal > RequiredDistance )
+	{
+		MoveForward();// Move towards the signal
+		//delay( MovementTimeRequired);//this might have to be inside the moving functions
+		//StopMoving();
+	}
+	else if( DistanceToSignal < RequiredDistance )
+	{
+		MoveBackward();// Move away from signal
+		//delay( MovementTimeRequired);//this might have to be inside the moving functions
+		//StopMoving();
+	}
 	                  
-     }
+}
 
 void TurnTo( double AngleToSignal )
 {
@@ -267,34 +298,39 @@ void TurnTo( double AngleToSignal )
 }   
 
           
-void ProperAnglePosition( double AngleToSignal) {
-		double DifferenceInAngle,
-               MovementTimeRequired;
+void ProperAnglePosition( double AngleToSignal)
+{
+	double DifferenceInAngle, MovementTimeRequired;
           
-		//following if statement for the car to be facing the tramsitter
-		  if(AngleToSignal > 90)
-			//turn right
-			DifferenceInAngle = (AngleToSignal - RequiredAngle);
-		  else if (AngleToSignal < 90)
-			//turn left
-			DifferenceInAngle = (RequiredAngle - AngleToSignal);
+	//following if statement for the car to be facing the tramsitter
+	if(AngleToSignal > 90)
+	{
+		//turn right
+		DifferenceInAngle = (AngleToSignal - RequiredAngle);
+	}
+	else //**MaybeRequired**if (AngleToSignal < 90)
+	{
+		//turn left
+		DifferenceInAngle = (RequiredAngle - AngleToSignal);
+	}
 
-		  MovementTimeRequired = CalculateAngleMoveTime(DifferenceInAngle);
+	MovementTimeRequired = CalculateAngleMoveTime(DifferenceInAngle);
 
-          TurnTo(AngleToSignal);// left and right may need to be swapped
+	TurnTo(AngleToSignal);// left and right may need to be swapped
                 		              
-	}   
+}   
 	
-double CalculateDistanceMoveTime(double DistanceToSignal){
-
-		return (abs(DistanceToSignal - RequiredDistance) / MOVEMENTSPEED);
-		//some shit that looks at the amplitude and phases and periods and shit
-	}
+double CalculateDistanceMoveTime(double DistanceToSignal)
+{
+	return (abs(DistanceToSignal - RequiredDistance) / MOVEMENTSPEED);
+	//some shit that looks at the amplitude and phases and periods and shit
+}
 	
-double CalculateAngleMoveTime(double DifferenceInAngle){
-		//car is facing the tramsitter
-		return (DifferenceInAngle/TURNSPEED*1000); //assumming the time is in Seconds
-	}
+double CalculateAngleMoveTime(double DifferenceInAngle)
+{
+	//car is facing the tramsitter
+	return (DifferenceInAngle/TURNSPEED*1000); //assumming the time is in Seconds
+}
 	
 
 double DetermineDistanceToSignal( void )
@@ -374,28 +410,35 @@ void testDelay(void){
     for (j=0; j<30000; j++); 
 }
 
-unsigned int GetADC (unsigned char channel)
+void SPIWrite(unsigned char value)
 {
-	unsigned int adc;
-	
-	SSIG=1;
-	SPCR=SPE|MSTR|CPOL|CPHA|SPR1|SPR0;
-	P1_4=0;
-	SPIWrite(0X01);
-	SPIWrite((channel*0x10)|0x80);
-	adc=((SPDR & 0x03)*0x100);
-	SPIWrite(0x55);
-	adc+=SPDR;
-	P1_4=1;
-	
-	return adc;
+	SPSTA&=(~SPIF); // Clear the SPIF flag in SPSTA
+	SPDAT=value;
+	while((SPSTA & SPIF)!=SPIF); //Wait for transmission to end
 }
 
-void SPIWrite (unsigned char value)
+unsigned int GetADC(unsigned char channel)
 {
-	SPIF=0;
-	SPDR=value;
-	while (SPIF==0);
+	unsigned int adc;
+
+	// initialize the SPI port to read the MCP3004 ADC attached to it.
+	SPCON&=(~SPEN); // Disable SPI
+	SPCON=MSTR|CPOL|CPHA|SPR1|SPR0|SSDIS;
+	SPCON|=SPEN; // Enable SPI
+	
+	P1_4=0; // Activate the MCP3004 ADC.
+	SPIWrite(channel|0x18);	// Send start bit, single/diff* bit, D2, D1, and D0 bits.
+	for(adc=0; adc<10; adc++); // Wait for S/H to setup
+	SPIWrite(0x55); // Read bits 9 down to 4
+	adc=((SPDAT&0x3f)*0x100);
+	SPIWrite(0x55);// Read bits 3 down to 0
+	P1_4=1; // Deactivate the MCP3004 ADC.
+	adc+=(SPDAT&0xf0); // SPDR contains the low part of the result. 
+	adc>>=4;
+	
+
+		
+	return adc;
 }
 
 double PhaseDifference(void)
--- Transmitter.c
+++ Transmitter.c
@@ -1,26 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <at89lp828.h>
+#include <at89lp51rd2.h>
 
-#define CLK 3686400L
+#define CLK 22118400L
 #define BAUD 115200L
-#define TIMER_2_RELOAD (0x10000L-(CLK/(16L*BAUD)))
+#define BRG_VAL (0x100-(CLK/(32L*BAUD)))
 
-#define FREQ 15000L
-#define TIMER0_RELOAD_VALUE (65536L-(CLK/(2L*FREQ)))
+#define FREQ 10000L
+#define TIMER0_RELOAD_VALUE (65536L-(CLK/(12L*FREQ)))
 
-void InitTimer0 (void)
+unsigned char _c51_external_startup(void)
 {
-	// Initialize timer 0 for ISR 'pwmcounter' below
+	// Configure ports as a bidirectional with internal pull-ups.
+	P0M0=0;	P0M1=0;
+	P1M0=0;	P1M1=0;
+	P2M0=0;	P2M1=0;
+	P3M0=0;	P3M1=0;
+	AUXR=0B_0001_0001; // 1152 bytes of internal XDATA, P4.4 is a general purpose I/O
+	P4M0=0;	P4M1=0;
+    
+    // Initialize the serial port and baud rate generator
+    PCON|=0x80;
+	SCON = 0x52;
+    BDRCON=0;
+    BRL=BRG_VAL;
+    BDRCON=BRR|TBCK|RBCK|SPD;
+	
+	// Initialize timer 0 for ISR 'pwmcounter()' below
 	TR0=0; // Stop timer 0
-	TMOD=(TMOD&0xf0)|0x01; // 16-bit timer
-	RH0=TIMER0_RELOAD_VALUE/0x100;
-	RL0=TIMER0_RELOAD_VALUE%0x100;
+	TMOD=0x01; // 16-bit timer
+	// Use the autoreload feature available in the AT89LP51RB2
+	// WARNING: There was an error in at89lp51rd2.h that prevents the
+	// autoreload feature to work.  Please download a newer at89lp51rd2.h
+	// file and copy it to the crosside\call51\include folder.
+	TH0=RH0=TIMER0_RELOAD_VALUE/0x100;
+	TL0=RL0=TIMER0_RELOAD_VALUE%0x100;
 	TR0=1; // Start timer 0 (bit 4 in TCON)
 	ET0=1; // Enable timer 0 interrupt
 	EA=1;  // Enable global interrupts
+    
+    return 0;
 }
-
 
 // Interrupt 1 is for timer 0.  This function is executed every time
 // timer 0 overflows: 100 us.
@@ -46,8 +66,6 @@ int test (unsigned char testNum) {
 void main (void)
 {
 	unsigned int f=15000, reload;
-    setbaud_timer2(TIMER_2_RELOAD); // Initialize serial port using timer 2 
-	InitTimer0(); // Initialize timer 0 and its interrupt
 	printf("\nFrequency test program\n");
 	while(1)
 	{	
